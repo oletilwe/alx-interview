0869610e6e4c23cc5d099aee47ae00baa34039f7
oletilwe <oletilwelephalala8@gmail.com>
2024-06-03 15:44:27 +0000

    new file

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,14 @@
+def canUnlockAll(boxes):
+    n = len(boxes)
+    opened = set()
+    stack = [0]
+
+    while stack:
+        box = stack.pop()
+        if box not in opened:
+            opened.add(box)
+            for key in boxes[box]:
+                if key < n and key not in opened:
+                    stack.append(key)
+
+    return len(opened) == n
