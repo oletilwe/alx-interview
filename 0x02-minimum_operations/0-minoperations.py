0b8c62e04ac4009f40ab4e9abfc66b645f0e2038
oletilwe <oletilwelephalala8@gmail.com>
2024-06-13 14:53:59 +0000

    new file

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
 def minOperations(n):
-    if n == 1:
+    if n <= 1:
         return 0
 
-    dp = [0] * (n + 1)
+    operations = 0
+    factor = 2
 
-    for i in range(2, n + 1):
-        dp[i] = i
+    while n > 1:
+        while n % factor == 0:
+            operations += factor
+            n //= factor
+        factor += 1
 
-        for j in range(i - 1, 1, -1):
-            if i % j == 0:
-                dp[i] = dp[j] + (i // j)
-                break
-
-    return dp[n]
+    return operations
